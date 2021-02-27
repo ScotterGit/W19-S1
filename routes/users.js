5fc431f56bae315dd8f8446789610d6925d002de
ScotterGit <scooterboots45@gmail.com>
2021-02-27 05:39:57 -0600

    req.body using YARC

--- routes/users.js
+++ routes/users.js
@@ -6,17 +6,22 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/form-submission', function(req, res, next) {
+  // console.log(req.body)
+  res.json({success=true});  
+});
+
 router.get('/:id/:firstName/:lastName', function(req, res, next) {
   console.log(req.params)
   res.send('yay')
 });
 
 
-// router.get('/john', function(req, res, next) {
-//   console.log(req.query)
-//   console.log(req.params)
-//   console.log(req.body)
-// });
+router.get('/john', function(req, res, next) {
+  console.log(req.query)
+  console.log(req.params)
+  console.log(req.body)
+});
 
 module.exports = router;
 
